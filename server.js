3dbd71c5373bb093bd863b8c166f0636de9a8be6
Been_Yan <asdfghjkl42045@gmail.com>
2022-02-25 01:02:58 +0800

    update

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-
+const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('Hello.');
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(port, () => {
     console.log(`[Message]: Server is listening on port ${port}`);
 });
