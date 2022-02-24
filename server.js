8b032f3aee015ca919892adbf0ba1da09832b7aa
Been_Yan <asdfghjkl42045@gmail.com>
2022-02-25 01:58:03 +0800

    update

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
 
+app.use('/css', express.static('views/public/css'));
+
 app.get('/', (req, res) => {
-    res.send('Hello.');
+    const options = {
+        root: __dirname + '/views/',
+    };
+    res.sendFile('login.html', options);
 });
 
 app.listen(port, () => {
